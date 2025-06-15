f90495199903675de8b21b50292bd50347592cb9
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-15 16:15:44 +0300

    create routes.config.ts

---
+++ src/routes.config.ts
@@ -0,0 +1,22 @@
+export const routesConfig = Object.freeze({
+	app: {
+		frontend: {
+			public: {
+				home: {
+					root: '/',
+					anchors: {
+						about: '#about',
+						experience: '#experience',
+						blog: '#blog'
+					}
+				}
+			}
+		},
+		backend: {}
+	},
+	external: {
+		linkedin: 'https://www.linkedin.com/in/shylo-volodymyr',
+		github: 'https://github.com/volodymyr-engineer',
+		codepen: 'https://codepen.io/mind-controle'
+	}
+});
