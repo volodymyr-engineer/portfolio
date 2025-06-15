c817cd447e10b2fa68783e66ddf82ae7108b83b3
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-15 04:38:13 +0300

    use the spotlight component in the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
+import { Spotlight } from 'src/components/spotlight';
 
 const LandingPage: NextPage = () => {
-	return <div />;
+	return (
+		<div className="h-full w-full">
+			<Spotlight />
+		</div>
+	);
 };
 
 export default LandingPage;
