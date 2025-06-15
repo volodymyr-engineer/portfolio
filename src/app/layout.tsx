ad2d2df523a213ca108482735de8e50e3dd24d15
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-15 04:37:13 +0300

    set up global styles for application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+				{children}
+			</body>
 		</html>
 	);
 }
