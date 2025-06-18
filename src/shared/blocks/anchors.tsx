b36347602bb48f380a6dec7bf8ad77e00bf9ebc6
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-18 23:56:18 +0300

    rename AnchorsBlock component to Anchors and update file path for consistency

--- src/shared/blocks/anchors.tsx
+++ src/shared/blocks/anchors.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from 'next-intl';
 import { Link } from 'src/i18n/navigation';
 import { routesConfig } from 'src/routes.config';
 
-export const AnchorsBlock = () => {
+export const Anchors = () => {
 	const translation = useTranslations('Header');
 
 	return (
@@ -31,19 +31,6 @@ export const AnchorsBlock = () => {
 						<span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none" />
 						<span className="nav-text text-xs font-bold tracking-widest text-slate-500 uppercase group-hover:text-slate-200 group-focus-visible:text-slate-200">
 							{translation('navigation-anchors.experience.label')}
-						</span>
-					</Link>
-				</li>
-				<li>
-					<Link
-						href={routesConfig.app.frontend.public.home.anchors.services}
-						role="navigation"
-						aria-label={translation('navigation-anchors.services.description')}
-						className="group flex items-center py-3"
-					>
-						<span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none" />
-						<span className="nav-text text-xs font-bold tracking-widest text-slate-500 uppercase group-hover:text-slate-200 group-focus-visible:text-slate-200">
-							{translation('navigation-anchors.services.label')}
 						</span>
 					</Link>
 				</li>
