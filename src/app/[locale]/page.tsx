e640308a94d2fe475c5b0e1c1d8584728212a0df
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-18 23:58:54 +0300

    add Experience block to homepage and update component imports for consistency

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,25 +1,27 @@
 import type { NextPage } from 'next';
-import { Spotlight } from 'src/components/spotlight';
-import { HeaderBlock } from 'src/components/header';
-import { AnchorsBlock } from 'src/components/anchors';
-import { SocialMedia } from 'src/components/social-media';
-import { AboutMeSection } from 'src/components/about-me';
+import { SpotlightEffect } from 'src/shared/components/spotlight-effect';
+import { Header } from 'src/shared/blocks/header';
+import { Anchors } from 'src/shared/blocks/anchors';
+import { SocialMedia } from 'src/shared/blocks/social-media';
+import { AboutMe } from 'src/shared/blocks/about-me';
+import { Experience } from 'src/shared/blocks/experience';
 
 const HomePage: NextPage = () => {
 	return (
 		<>
-			<Spotlight />
+			<SpotlightEffect />
 			<div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
 				<div className="lg:flex lg:justify-between lg:gap-4">
 					<header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
 						<div>
-							<HeaderBlock />
-							<AnchorsBlock />
+							<Header />
+							<Anchors />
 						</div>
 						<SocialMedia />
 					</header>
 					<main className="pt-24 lg:w-[52%] lg:py-24">
-						<AboutMeSection />
+						<AboutMe />
+						<Experience />
 					</main>
 				</div>
 			</div>
