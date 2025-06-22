a4cbfab4e1377c376f702367143a12cc3159357b
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-22 22:19:16 +0300

    add metadata generation with localized SEO support and include Google Search Console verification

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,71 @@
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
-import { getMessages, setRequestLocale } from 'next-intl/server';
+import { getMessages, setRequestLocale, getTranslations } from 'next-intl/server';
 import { Inter } from 'next/font/google';
 import { notFound } from 'next/navigation';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import { routing } from 'src/i18n/routing';
+import { routesConfig } from 'src/routes.config';
 import 'src/theme.config.css';
+
+type MetadataProps = {
+	params: Promise<{ locale: string }>;
+};
 
 type LocaleLayoutProps = {
 	children: Readonly<ReactNode>;
 	params: Promise<{ locale: string }>;
 };
 
-export const metadata: Metadata = {
-	title: 'Volodymyr Shylo - JavaScript Engineer',
-	description: ''
-};
+export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
+	const { locale } = await params;
+	const translate = await getTranslations({ locale: locale, namespace: 'SEO.metadata' });
+	const baseUrl = `${process.env.NEXT_PUBLIC_HTTP_PROTOCOL}://${process.env.NEXT_PUBLIC_HTTP_DOMAIN}`;
+
+	return {
+		referrer: 'origin',
+		title: translate('title'),
+		applicationName: translate('application-name'),
+		description: translate('description'),
+		keywords: translate('keywords'),
+		category: translate('category'),
+		verification: {
+			google: process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE_VERIFICATION_CODE
+		},
+		manifest: `${baseUrl}/manifest.json`,
+		openGraph: {
+			type: 'website',
+			url: baseUrl,
+			countryName: translate('open-graph.country-name'),
+			locale: locale,
+			siteName: process.env.NEXT_PUBLIC_HTTP_DOMAIN,
+			title: translate('open-graph.title'),
+			description: translate('open-graph.description'),
+			images: {
+				type: 'image/jpeg',
+				url: `${baseUrl}/assets/metadata/open-graph.logo.webp`,
+				alt: translate('open-graph.image.alt'),
+				width: 525,
+				height: 300
+			}
+		},
+		twitter: {
+			title: translate('twitter.title'),
+			description: translate('twitter.description'),
+			site: process.env.NEXT_PUBLIC_HTTP_DOMAIN,
+			images: {
+				url: `${baseUrl}/assets/metadata/twitter.logo.webp`,
+				alt: translate('twitter.image.alt'),
+				width: 525,
+				height: 300
+			}
+		},
+		authors: {
+			name: translate('authors.name'),
+			url: routesConfig.external.socialMedia.linkedin
+		}
+	};
+}
 
 const inter = Inter({
 	variable: '--font-inter',
