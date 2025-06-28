41e6c7b757ea052c61ee004e92f68b5355d4dcf5
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-29 01:27:07 +0300

    mark as static page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,6 +8,8 @@ import { Values } from 'src/shared/blocks/values';
 import { Blog } from 'src/shared/blocks/blog';
 import { PortalAnimation } from 'src/shared/components/portal-animation';
 
+export const dynamic = 'force-static';
+
 const HomePage: NextPage = () => (
 	<div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
 		<div className="lg:flex lg:justify-between lg:gap-4">
