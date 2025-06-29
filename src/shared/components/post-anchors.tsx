aed60062fc5de797ae40578d03ab0ca54d658e7d
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-06-29 15:33:09 +0300

    create component for Posts

---
+++ src/shared/components/post-anchors.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'src/i18n/navigation';
+
+type Props = {
+	title: Readonly<string>;
+	anchors: ReadonlyArray<{
+		href: Readonly<string>;
+		label: Readonly<string>;
+		ariaLabel: Readonly<string>;
+	}>;
+};
+
+export const PostAnchors = (props: Props) => (
+	<>
+		<h2 className="mt-10 ml-6 text-lg font-bold text-slate-400 uppercase">{props.title}</h2>
+		<ul className="mt-2 ml-6">
+			{props.anchors.map((anchor, index) => (
+				<li key={index} className="w-fit">
+					<Link href={`#${anchor.href}`} role="navigation" aria-label={anchor.ariaLabel} className="group flex items-center py-3">
+						<span className="nav-indicator mr-4 h-px w-8 bg-slate-400 transition-all group-hover:w-16 group-focus-visible:w-16 motion-reduce:transition-none" />
+						<span className="nav-text text-xs font-bold tracking-widest text-slate-400 uppercase">{anchor.label}</span>
+					</Link>
+				</li>
+			))}
+		</ul>
+	</>
+);
