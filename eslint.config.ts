f717ef3813a634851c65d06345d90da8bc2cb17c
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-07-05 01:18:34 +0300

    update

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,27 +10,27 @@ const compat = new FlatCompat({
 
 const eslintConfig: Linter.Config[] = [
 	...compat.extends('next/core-web-vitals', 'next/typescript'),
-	{
-		files: ['**/*.tsx'],
-		plugins: {
-			'jsx-a11y': jsxA11yPlugin
-		},
-		rules: {
-			...jsxA11yPlugin.configs.recommended.rules
-		}
-	},
-	{
-		files: ['**/*.ts', '**/*.tsx'],
-		rules: {
-			'no-console': 'error',
-			'react/jsx-max-props-per-line': [
-				'error',
-				{
-					maximum: 2
-				}
-			]
-		}
-	},
+	// {
+	// 	files: ['**/*.tsx'],
+	// 	plugins: {
+	// 		'jsx-a11y': jsxA11yPlugin
+	// 	},
+	// 	rules: {
+	// 		...jsxA11yPlugin.configs.recommended.rules
+	// 	}
+	// },
+	// {
+	// 	files: ['**/*.ts', '**/*.tsx'],
+	// 	rules: {
+	// 		'no-console': 'error',
+	// 		'react/jsx-max-props-per-line': [
+	// 			'error',
+	// 			{
+	// 				maximum: 2
+	// 			}
+	// 		]
+	// 	}
+	// },
 	{
 		files: ['**/*.tsx'],
 		rules: {
