e762e49c1a43bbcd775dddef56a93e2ae9c556d8
Volodymyr Shylo <electronic.commerce.startup@gmail.com>
2025-07-04 19:48:00 +0300

    uncomment eslint and env config implementations in next.config.ts and eslint.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,29 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-// import ESLintPlugin from 'eslint-webpack-plugin';
-// import * as envalid from 'envalid';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import * as envalid from 'envalid';
 import type { NextConfig } from 'next';
 
-// export const validationEnvFile = () => {
-// 	envalid.cleanEnv(process.env, {
-// 		NEXT_PUBLIC_HTTP_PROTOCOL: envalid.str(),
-// 		NEXT_PUBLIC_HTTP_DOMAIN: envalid.str(),
-// 		NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE_VERIFICATION_CODE: envalid.str()
-// 	});
-// };
+export const validationEnvFile = () => {
+	envalid.cleanEnv(process.env, {
+		NEXT_PUBLIC_HTTP_PROTOCOL: envalid.str(),
+		NEXT_PUBLIC_HTTP_DOMAIN: envalid.str(),
+		NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE_VERIFICATION_CODE: envalid.str()
+	});
+};
 
 const nextConfig: NextConfig = {
-	// reactStrictMode: true,
+	reactStrictMode: true,
 
 	webpack: (config) => {
-		// validationEnvFile();
-		//
-		// config.plugins.push(
-		// 	new ESLintPlugin({
-		// 		extensions: ['ts', 'tsx'],
-		// 		exclude: ['node_modules'],
-		// 		failOnError: true
-		// 	})
-		// );
+		validationEnvFile();
+
+		config.plugins.push(
+			new ESLintPlugin({
+				extensions: ['ts', 'tsx'],
+				exclude: ['node_modules'],
+				failOnError: true
+			})
+		);
 
 		return config;
 	}
